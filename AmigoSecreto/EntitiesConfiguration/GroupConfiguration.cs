c366b86f3cfc6f248a101d86b65a389acd73c2b7
jvmds <joaovitormelodossantos.1@gmail.com>
2024-12-06 07:29:01 -0300

    feat: configuração das entidades e contexto

---
+++ AmigoSecreto/EntitiesConfiguration/GroupConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using AmigoSecreto.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AmigoSecreto.EntitiesConfiguration;
+
+public class GroupConfiguration : IEntityTypeConfiguration<GroupEntity>
+{
+    public void Configure(EntityTypeBuilder<GroupEntity> builder)
+    {
+        builder
+            .HasMany(g => g.Users)
+            .WithOne(u => u.Group)
+            .HasForeignKey(u => u.GroupId);
+        builder
+            .Property(g => g.Name)
+            .HasMaxLength(50);
+        builder
+            .Property(g => g.Description)
+            .HasMaxLength(100);
+    }
+}
---
+++ AmigoSecreto/EntitiesConfiguration/UserConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using AmigoSecreto.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AmigoSecreto.EntitiesConfiguration;
+
+public class UserConfiguration : IEntityTypeConfiguration<UserEntity>
+{
+    public void Configure(EntityTypeBuilder<UserEntity> builder)
+    {
+        builder
+            .HasOne(u => u.SecretSanta)
+            .WithOne()
+            .HasForeignKey<UserEntity>(u => u.SecretSantaId)
+            .IsRequired(false);
+        builder
+            .Property(u => u.Email)
+            .HasAnnotation("ValidationType", "Email");
+        builder
+            .Property(u => u.FirstName)
+            .HasMaxLength(50);
+        builder
+            .Property(u => u.LestName)
+            .HasMaxLength(50);
+    }
+}
